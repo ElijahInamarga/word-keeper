369435b533b54b46f6b818fbbc8bbb67b8fb75a5
ElijahInamarga <elijahcek@gmail.com>
2025-11-27 20:03:27 -0800

    Module template successfully loading and unloading with print statements

--- word_keeper.c
+++ word_keeper.c
@@ -1,18 +1,20 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
-static __init int init(void)
+static __init int my_init(void)
 {
+  pr_info("word_keeper - Module loaded\n");
   return 0;
 }
 
-static void __exit exit(void)
+static void __exit my_exit(void)
 {
 
+  pr_info("word_keeper - Module unloaded\n");
 }
 
-module_init(init);
-module_exit(exit)
+module_init(my_init);
+module_exit(my_exit)
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Elijah Inamarga");
